342a74fe3c50fd5359a64797817a2c71323380ae
korede <149403541+korede90@users.noreply.github.com>
2025-03-13 11:49:53 +0100

    suddo

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Install Tesseract OCR
-apt-get update && apt-get install -y tesseract-ocr
+
+# Install Tesseract
+sudo apt-get update && sudo apt-get install -y tesseract-ocr
 
 # Install Python dependencies
 pip install -r requirements.txt
